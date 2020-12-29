da48b56a7b8dd52eddb52fd979a49a01f6d289b2
v_yameiwu <v_yameiwu@tencent.com>
2020-12-29 19:25:44 +0800

    feat: 归并排序

--- nlogn排序(快排，重点).js
+++ nlogn排序(快排，重点).js
@@ -60,3 +60,49 @@ var quickSort = (array, start, end) => {
   // 调用方式
   return sort(nums, 0, nums.length - 1);
 };
+
+// 2.归并排序
+var mergeSort = function (nums) {
+  return sort(nums, 0, nums.length - 1);
+
+  function sort(array, left, right) {
+    if (left >= right) {
+      return array;
+    }
+    var mid = (left + right) >> 1;
+    sort(array, left, mid);
+    sort(array, mid + 1, right);
+    merge(array, left, mid, right);
+    return array;
+  }
+
+  // 合并两个有序链表
+  function merge(arr, left, mid, right) {
+    // arr 排序数组
+    // left: 当前处理的第一项元素坐标
+    // mid：当前处理的中间元素坐标
+    // right：当前处理的最后一项元素坐标
+    var i = left; // 做数组的起始坐标 [left, mid]
+    var j = mid + 1; // 右数组的起始坐标 [mid + 1, right]
+    var k = 0; // 标记存入temp的个数
+    var temp = [];
+
+    while (i <= mid && j <= right) {
+      temp[k++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+    }
+
+    // 走到这里说明左边或者右边有一个数组取完了，则将未取完的也放到temp中
+    while (i <= mid) {
+      temp[k++] = arr[i++];
+    }
+
+    while (j <= right) {
+      temp[k++] = arr[j++];
+    }
+
+    // 将temp的元素拷贝回arr去
+    for (var p = 0; p < temp.length; p++) {
+      arr[left + p] = temp[p];
+    }
+  }
+};
--- 合并两个有序链表.js
+++ 合并两个有序链表.js
@@ -24,11 +24,31 @@ var mergeTwoLists = (l1, l2) => {
 
   // 改变next 指针的指向之后，一定要返回结果链表，返回的链表是一个
   // 这里的分支只能是 if else 必须走一个分支
-  if (l1.value < l2.value) {
+  if (l1.val < l2.val) {
     l1.next = mergeTwoLists(l1.next, l2);
     return l1;
   } else {
     l2.next = mergeTwoLists(l2.next, l1);
     return l2;
   }
+};
+
+// 合并两个有序数组
+var mergeTwoArray = function (l1, l2) {
+  var i = 0;
+  var j = 0;
+  var k = 0; // 标记存入res的个数
+  var res = [];
+  while (i <= l1.length - 1 && j <= l2.length - 1) {
+    res[k++] = l1[i] < l2[j] ? l1[i++] : l2[j++];
+  }
+  // 到这里说明了l1, l2中有一个已经全部取完
+  while (i < l1.length) {
+    res[k++] = l1[i++];
+  }
+
+  while (j < l2.length) {
+    res[k++] = l2[j++];
+  }
+  return res;
 };
