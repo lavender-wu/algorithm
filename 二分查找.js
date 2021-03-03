8599a952fd5e548dba18da3cbe43ac55edddb9c2
v_yameiwu <v_yameiwu@tencent.com>
2021-03-04 01:26:09 +0800

    feat: 增加二分查找

---
+++ 二分查找.js
@@ -0,0 +1,38 @@
+// 二分查找
+var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length;
+  let mid;
+  while(left <= right) {
+    // 更新中点坐标
+    mid = Math.floor(left + (right - left) / 2);
+    // 找到下标
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+    // 中点数偏小
+      left = mid + 1;
+    } else {
+    // 中点数偏大
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+// 两边往中间缩进
+var search2 = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while(left <= right) {
+    if (nums[left] === target) {
+      return left;
+    }
+    if (nums[right] === target) {
+      return right;
+    }
+    left++;
+    right--;
+  }
+  return -1;
+};
