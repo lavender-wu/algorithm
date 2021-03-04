e617fdf2686faf266cc0b913da94b598d3d33179
v_yameiwu <v_yameiwu@tencent.com>
2021-03-04 11:00:24 +0800

    feat: 二分查找纠正右下标

--- 二分查找.js
+++ 二分查找.js
@@ -1,30 +1,30 @@
 // 二分查找
-var search = function(nums, target) {
+var search = function (nums, target) {
   let left = 0;
-  let right = nums.length;
+  let right = nums.length - 1;
   let mid;
-  while(left <= right) {
+  while (left <= right) {
     // 更新中点坐标
     mid = Math.floor(left + (right - left) / 2);
     // 找到下标
     if (nums[mid] === target) {
       return mid;
     } else if (nums[mid] < target) {
-    // 中点数偏小
+      // 中点数偏小
       left = mid + 1;
     } else {
-    // 中点数偏大
+      // 中点数偏大
       right = mid - 1;
     }
   }
   return -1;
-}
+};
 
 // 两边往中间缩进
-var search2 = function(nums, target) {
+var search2 = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
-  while(left <= right) {
+  while (left <= right) {
     if (nums[left] === target) {
       return left;
     }
